1e07aa04591de2cc7186149ad3786c94d4f6bf14
dk872 <dimakulyk2005@gmail.com>
2025-11-11 16:37:23 +0200

    Add ArraySummer class

---
+++ src/main/java/org/example/ArraySummer.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.concurrent.RecursiveTask;
+
+public class ArraySummer extends RecursiveTask<Long> {
+    private static final int THRESHOLD = 20;
+    private final int[] array;
+    private final int start;
+    private final int end;
+
+    public ArraySummer(int[] array, int start, int end) {
+        this.array = array;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Long compute() {
+        int length = end - start;
+
+        if (length < THRESHOLD) {
+            return computeDirectly();
+        }
+
+        int middle = start + length / 2;
+
+        ArraySummer leftTask = new ArraySummer(array, start, middle);
+        ArraySummer rightTask = new ArraySummer(array, middle, end);
+
+        leftTask.fork();
+        rightTask.fork();
+        Long rightResult = rightTask.join();
+        Long leftResult = leftTask.join();
+
+        return leftResult + rightResult;
+    }
+
+    private long computeDirectly() {
+        long sum = 0;
+        for (int i = start; i < end; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+}
